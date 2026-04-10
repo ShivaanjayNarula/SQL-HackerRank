3783da58f24849db923e9a49e74083f22940441a
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2026-04-10 19:24:35 +0530

    Create 026) Ollivander's Inventory.sql

---
+++ 026) Ollivander's Inventory.sql
@@ -0,0 +1,12 @@
+SELECT A.ID, B.AGE, A.COINS_NEEDED, A.POWER
+FROM WANDS A
+    JOIN WANDS_PROPERTY B
+    ON A.CODE = B.CODE
+    WHERE B.IS_EVIL = 0
+    AND A.COINS_NEEDED = (SELECT MIN(A1.COINS_NEEDED)
+                            FROM WANDS A1
+                            JOIN WANDS_PROPERTY B1
+                            ON A1.CODE = B1.CODE
+                            WHERE B.AGE = B1.AGE
+                            AND A.POWER = A1.POWER)
+ORDER BY A.POWER DESC, B.AGE DESC;
