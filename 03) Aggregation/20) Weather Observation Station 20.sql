73549399ebd59675ffa4a0578084a244e651441c
Shivaanjay Narula <shivaanjaynarula@gmail.com>
2026-04-12 21:43:37 +0530

    Create 20) Weather Observation Station 20.sql

---
+++ 03) Aggregation/20) Weather Observation Station 20.sql
@@ -0,0 +1,8 @@
+SELECT ROUND(AVG(LAT_N), 4)
+FROM (
+    SELECT LAT_N,
+           ROW_NUMBER() OVER (ORDER BY LAT_N) AS RN,
+           COUNT(*) OVER () AS TOTAL
+    FROM STATION
+) T
+WHERE RN IN (FLOOR((total + 1) / 2), FLOOR((TOTAL + 2) / 2));
